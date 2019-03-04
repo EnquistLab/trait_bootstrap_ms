72f421722f27a973fdee03c9a7e9472bc8510e2a
bmaitner <bmaitner@gmail.com>
2019-03-04 12:02:36 -0700

    code to calculate proportion of the CIs that contain the true value

--- r_functions/prop_correct_cis.R
+++ r_functions/prop_correct_cis.R
@@ -113,6 +113,43 @@ proportion_ci_correct_proportional_sampling<-function(simulation_output){
   }  
   
   
+  output$method<-"nonparametric_bootstrapping"  
+  
+  output_p<-unique(simulation_output[c('trait','site','moment','sample_proportion','total_samples')])  
+  output_p$proportion_ci95_correct<-NA  
+  output_p$proportion_ci100_correct<-NA
+  
+  
+  for(i in 1:nrow(output_p)){
+    
+    output_p$proportion_ci95_correct[i]<-
+      sum(simulation_output$in_95ci[which(simulation_output$sample_proportion==output$sample_proportion[i] &
+                                            simulation_output$trait==output$trait[i] &
+                                            simulation_output$site==output$site[i] &
+                                            simulation_output$moment==output$moment[i])])/length(which(simulation_output$sample_proportion==output$sample_proportion[i] &
+                                                                                                         simulation_output$trait==output$trait[i] &
+                                                                                                         simulation_output$site==output$site[i] &
+                                                                                                         simulation_output$moment==output$moment[i]))    
+    
+    output_p$proportion_ci100_correct[i] <- 
+      sum(simulation_output$in_100ci[which(simulation_output$sample_proportion==output$sample_proportion[i] &
+                                             simulation_output$trait==output$trait[i] &
+                                             simulation_output$site==output$site[i] &
+                                             simulation_output$moment==output$moment[i])])/
+      length(which(simulation_output$sample_proportion==output$sample_proportion[i] & 
+                     simulation_output$trait==output$trait[i] &
+                     simulation_output$site==output$site[i] &
+                     simulation_output$moment==output$moment[i]))    
+    
+    
+  }  
+  
+  
+  output_p$method<-"parametric_bootstrapping"  
+  
+  output<-rbind(output,output_p)
+  
+  
   
   return(output)  
   
