865cfdd4dc9aeb290338506d317e579ab008867d
Brian <bmaitner@gmail.com>
2021-04-19 12:19:07 -0400

    updated code to be more explicit about default settings

--- r_functions/sim_sample_size.R
+++ r_functions/sim_sample_size.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 # Note that this applies sample sizes at the species x site level.
 # This function assumes that one ID is one sample.  
-  # So assigning IDs to individuals could return multiple samples per individula.
+  # So assigning IDs to individuals could return multiple samples per individual.
   # Assigning IDs to traits taken from the same organ returns 
   # multiple traits form the same organ (e.g. leaf)
 
@@ -13,7 +13,9 @@ sim_sample_size <- function(tidy_traits,
                             n_reps_trait = 10, #controls the number of replicated draws for each  sample size
                             n_reps_boot = 200, #number of bootstrap replicates to use
                             seed = 2005, #set seed for reproducibility.  2005 = Year Transformers: The Movie (cartoon version) is set.
-                            prob = NULL #Probability for biased sampling.  If NULL, sampling will be random.
+                            prob = NULL, ##Probability for biased sampling.  If NULL, sampling will be random.
+                            distribution_type = "normal", #distribution for fitting the parametric
+                            min_n_in_sample = 1
 ){
 
   
@@ -41,7 +43,6 @@ for( n in n_to_sample){
     }
     
     
-    
     #Get species mean traits
     species_means_nt <- samples_to_means(tidy_traits = traits_nt,
                                          level = "taxon")
@@ -62,7 +63,7 @@ for( n in n_to_sample){
                    trait_col = "trait",
                    value_col = "value",
                    abundance_col = "abundance",
-                   min_n_leaves = 1)
+                   min_n_in_sample = min_n_in_sample)
     
     #Trait imputation for species mean
     imputed_species_mean <- 
@@ -74,7 +75,7 @@ for( n in n_to_sample){
                    trait_col = "trait",
                    value_col = "value",
                    abundance_col = "abundance",
-                   min_n_leaves = 1)
+                   min_n_in_sample = 1)
     
     #Trait imputation for species x site mean
     imputed_species_x_site_mean <- 
@@ -86,7 +87,7 @@ for( n in n_to_sample){
                    trait_col = "trait",
                    value_col = "value",
                    abundance_col = "abundance",
-                   min_n_leaves = 1)
+                   min_n_in_sample = 1)
     
     #Get Non-parametric moments
     np_results_nt <- 
@@ -120,7 +121,7 @@ for( n in n_to_sample){
     #Get parametric moments
     pbs_results_nt <-
       trait_fit_distributions(imputed_traits = imputed_full,
-                              distribution_type = "normal")
+                              distribution_type = distribution_type)
       
     pbs_results_nt <-
       trait_parametric_bootstrap(fitted_distributions = pbs_results_nt,
