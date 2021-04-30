0de68277b436c43cbcc430e06cb474a29c4cbf70
TanyaS08 <tanya.strydom@icloud.com>
2021-04-30 18:52:55 +0200

    A

--- scripts_for_ms_with_traitstrap/7_global_mean.R
+++ scripts_for_ms_with_traitstrap/7_global_mean.R
@@ -58,9 +58,9 @@ global <- rbind(readRDS("output_data/local_intra_rmbl_moments_2010.rds") %>%
               pivot_longer(cols = c(SLA, height),
                            names_to = 'trait',
                            values_to = 'true_value') %>%
-              mutate(true_value = ifelse(trait == "SLA",
-                                         true_value/100,
-                                         true_value)) %>%
+              # mutate(true_value = ifelse(trait == "SLA",
+              #                            true_value/100,
+              #                            true_value)) %>%
               group_by(site, trait) %>%
               summarise(mean = mean(true_value, na.rm = TRUE),
                         variance = var(true_value, na.rm = TRUE),
@@ -106,7 +106,7 @@ for (i in 1:2) {
                    colour = dataset,
                    shape = level),
                size = 1.3,
-               alpha = 0.6) +
+               alpha = 0.9) +
     facet_grid(rows = vars(moment),
                cols = vars(trait),
                scales = 'free',
@@ -164,6 +164,13 @@ for (i in 1:2) {
 }
 
 global_plots[[1]] +
+  inset_element(img1,
+                left = 0.0,
+                bottom = 0.93,
+                right = 0.07,
+                top = 0.99, 
+                align_to = 'full', 
+                ignore_tag = TRUE) + theme_void() +
   global_plots[[2]] +
   theme(
     strip.text.y = element_blank(),
@@ -176,4 +183,172 @@ global_plots[[1]] +
 ggsave(here::here("figures/global_mean.png"),
        height = 120, width = 180,
        units = "mm", dpi = 600)
+
+####
+
+global = 
+  readRDS("output_data/global_vs_local_summary.rds") %>%
+  pivot_longer(cols = c('mean', 'var', 'skew', 'kurt'),
+               names_to = 'moment',
+               values_to = 'estimate') %>%
+  pivot_longer(cols = contains('ci_high'),
+               names_to = 'ci_high_moment',
+               values_to = 'ci_high') %>%
+  mutate(ci_high_moment = str_to_lower(str_extract(ci_high_moment,'[[:alpha:]]*$'))) %>%
+  filter(ci_high_moment == moment) %>%
+  pivot_longer(cols = contains('ci_low'),
+               names_to = 'ci_low_moment',
+               values_to = 'ci_low') %>%
+  mutate(ci_low_moment = str_to_lower(str_extract(ci_low_moment,'[[:alpha:]]*$'))) %>%
+  filter(ci_low_moment == moment) %>%
+  mutate(moment = case_when(moment == 'var' ~ 'variance',
+                            moment == 'kurt' ~ 'kurtosis',
+                            moment == 'skew' ~ 'skewness',
+                            TRUE ~ moment),
+         method = case_when(method == 'global cwm' ~ 'Cross-Site CW',
+                            method == 'site-specic CWM' ~ 'Site-Specific CW',
+                            method == 'nonparametric bs' ~ 'Non-Parametric BS',
+                            method == 'parametric bs' ~ 'Parametric BS',
+                            TRUE ~ method)) %>%
+  select(-c(global, ci_low_moment, ci_high_moment)) %>%
+  mutate(method = ordered(method,levels = c("Cross-Site CW",
+                                            "Site-Specific CW",
+                                            "Parametric BS",
+                                            "Non-Parametric BS"))) %>%
+  left_join(.,
+            simdata %>%
+              select(site, trait, moment, true_value)) %>%
+  mutate(moment = ordered(moment,levels = c("mean",
+                                            "variance",
+                                            "skewness",
+                                            "kurtosis"))) %>%
+  left_join(.,
+            readRDS("data/elevations.RDS")) %>%
+  distinct(trait_source, method, site, trait, moment, n, estimate,
+           ci_high, ci_low, true_value, mean_elev)
+
+
+####
+
+
+ggplot(global) +
+  geom_line(aes(x = mean_elev,
+                y = true_value),
+            linetype = 2,
+            colour = "grey69") +
+  # geom_ribbon(aes(x = mean_elev,
+  #                 ymin = ci_low,
+  #                 ymax = ci_high,
+  #                 fill = method,
+  #                 group = trait_source),
+  #             alpha = 0.2) +
+  geom_line(aes(x = mean_elev,
+                y = estimate,
+                colour = method, 
+                linetype = trait_source),
+            size = 1.3) +
+  facet_grid(rows = vars(moment),
+             cols = vars(trait),
+             labeller = labeller(
+               trait = traits_parsed,
+               .default = capitalize
+             ),
+             switch = 'y',
+             scales = 'free') +
+  scale_fill_manual(guide = guide_legend(title = "Method",
+                                         title.position="top",
+                                         title.hjust = 0.5),
+                    values = pal_df$c,
+                    labels = pal_df$l) +
+  scale_colour_manual(guide = guide_legend(title = "Method",
+                                           title.position="top",
+                                           title.hjust = 0.5),
+                      values = pal_df$c,
+                      labels = pal_df$l) +
+  theme_moon
+
+global_plots <- vector('list', 2)
+
+trait_select = simdata %>%
+  distinct(trait) %>%
+  pull()
+
+for (i in 1:2) {
+  
+  global_plots[[i]] <-
+  readRDS("output_data/global_vs_local_summary.rds") %>%
+    pivot_longer(cols = c('mean', 'var', 'skew', 'kurt'),
+                 names_to = 'moment',
+                 values_to = 'estimate') %>%
+    pivot_longer(cols = contains('ci_high'),
+                 names_to = 'ci_high_moment',
+                 values_to = 'ci_high') %>%
+    mutate(ci_high_moment = str_to_lower(str_extract(ci_high_moment,'[[:alpha:]]*$'))) %>%
+    filter(ci_high_moment == moment) %>%
+    pivot_longer(cols = contains('ci_low'),
+                 names_to = 'ci_low_moment',
+                 values_to = 'ci_low') %>%
+    mutate(ci_low_moment = str_to_lower(str_extract(ci_low_moment,'[[:alpha:]]*$'))) %>%
+    filter(ci_low_moment == moment) %>%
+    mutate(moment = case_when(moment == 'var' ~ 'variance',
+                              moment == 'kurt' ~ 'kurtosis',
+                              moment == 'skew' ~ 'skewness',
+                              TRUE ~ moment)) %>%
+    select(-c(global, ci_low_moment, ci_high_moment)) %>%
+    pivot_longer(cols = contains('true'),
+                 names_to = 'true_moment',
+                 values_to = 'true_value') %>%
+    mutate(true_moment = str_to_lower(str_extract(true_moment,'[[:alpha:]]*$'))) %>%
+    filter(true_moment == moment) %>%
+    select(-c(true_moment)) %>%
+    mutate(moment = ordered(moment,levels = c("mean",
+                                              "variance",
+                                              "skewness",
+                                              "kurtosis"))) %>%
+    left_join(.,
+              readRDS("data/elevations.RDS")) %>%
+    filter(trait == trait_select[i]) %>%
+    ggplot() +
+    geom_line(aes(x = mean_elev,
+                  y = true_value),
+              linetype = 2,
+              colour = "grey69") +
+    geom_ribbon(aes(x = mean_elev,
+                    ymin = ci_low,
+                    ymax = ci_high,
+                    fill = source),
+                alpha = 0.2) +
+    geom_line(aes(x = mean_elev,
+                  y = estimate,
+                  colour = source)) +
+    facet_grid(rows = vars(moment),
+               cols = vars(trait),
+               labeller = labeller(
+                 trait = traits_parsed,
+                 .default = capitalize
+               ),
+               switch = 'y',
+               scales = 'free') +
+    theme_moon
+  
+}
+
+global_plots[[1]] +
+  global_plots[[2]] +
+  theme(
+    strip.text.y = element_blank(),
+    axis.title.y = element_blank()) +
+  global_plots[[3]] +
+  theme(
+    strip.text.y = element_blank(),
+    axis.title.y = element_blank()) +
+  global_plots[[4]]  +
+  global_plots[[5]] +
+  theme(
+    strip.text.y = element_blank(),
+    axis.title.y = element_blank()) +
+  plot_layout(guides = 'collect') +
+  plot_annotation(theme = theme(
+    plot.background = element_rect(fill = "white", colour = NA),
+    legend.position = 'bottom'))
 
