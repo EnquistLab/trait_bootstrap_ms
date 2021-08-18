96328afb9ef62c1d1b8c7ce22d8730922f19c38e
Tanya Strydom <tanya.strydom@icloud.com>
2021-08-18 21:25:31 +0200

    something looks weird...

--- scripts_for_ms_with_traitstrap/10_relative_rank.R
+++ scripts_for_ms_with_traitstrap/10_relative_rank.R
@@ -63,3 +63,65 @@ ggsave(here::here("figures/Rel_rank.png"),
 
 #### Lollipop style plotting ####
 
+herb_means = 
+  herbs %>%
+  filter(sample_size == 9) %>%
+  group_by(site, trait, moment, method) %>%
+  summarise(estimate = mean(deviation),
+            true_val = mean(true_value))
+
+herb_means_long = 
+  herb_means %>%
+  pivot_longer(cols = c(estimate, true_val),
+               names_to = "point_val", 
+               values_to = "value")
+
+herb_means_true = 
+  herb_means %>%
+  group_by(trait, moment) %>%
+  summarise(true_val = mean(true_val))
+
+ggplot(herb_means_true) +
+  geom_vline(aes(xintercept = true_val),
+             color = "grey69",
+             size = 0.6) +
+  geom_segment(data = herb_means,
+               aes(x = true_val,
+                   xend = estimate,
+                   y = paste(method, site),
+                   yend = paste(method, site),
+                   group = site),
+               color = "grey69",
+               size = 0.3) +
+  geom_point(data = herb_means_long,
+             aes(x = value,
+                 y = paste(method, site),
+                 colour = method,
+                 fill = method,
+                 shape = point_val),
+             size = 2) +
+  facet_grid(rows = vars(trait),
+             cols = vars(moment),
+             labeller = labeller(
+               .default = capitalize
+             ),
+             switch = 'y',
+             scales = 'free')  +
+  scale_fill_manual(guide = guide_legend(title = "Method",
+                                         override.aes = list(shape = 21)),
+                    values = pal_df$c,
+                    breaks = pal_df$l) +
+  scale_colour_manual(guide = guide_legend(title = "Method",
+                                           override.aes = list(shape = 21)),
+                      values = colorspace::darken(pal_df$c, 0.5),
+                      breaks = pal_df$l) +
+  scale_alpha_discrete(guide = guide_legend(title = "Value in CI",
+                                            override.aes = list(shape = 16,
+                                                                size = 3)),
+                       range = c(0.5, 0.9)) +
+  labs(
+    x = "Deviation from true value",
+    y = NULL
+  ) +
+  #guides(size = 'none') +
+  theme_lollipop
