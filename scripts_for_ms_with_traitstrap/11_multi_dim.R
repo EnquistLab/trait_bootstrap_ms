b93f686e7a56ecb09d9e430aab970d2ce04ff734
Tanya Strydom <tanya.strydom@icloud.com>
2022-08-29 15:07:30 -0400

    🚑

--- scripts_for_ms_with_traitstrap/11_multi_dim.R
+++ scripts_for_ms_with_traitstrap/11_multi_dim.R
@@ -107,181 +107,169 @@ multi_rarity =
          hit = ifelse(ci_low <= true_value & true_value <= ci_high,
                       'Yes',
                       'No')) %>%
+  mutate(measure = case_when(measure == "avg_uniqueness" ~ "Mean uniqueness",
+                             measure == "avg_distinctiveness" ~ "Mean distinctiveness")) %>%
   mutate(deviation = ifelse(overunder == "under",
                             -1*deviation,
                             deviation),
          measure = ordered(measure,
-                           levels = c('avg_uniqueness', 'avg_distinctiveness')))
+                           levels = c('Mean uniqueness', 'Mean distinctiveness')))
 
 #### Plotting moons ----
 
-data <- vector('list', 2)
-data[[1]] = multi_method
-data[[2]] = multi_rarity
-inset_plots <- vector('list', 2)
-moon_plots <- vector('list', 2)
+overunders = 
+  rbind(multi_method,
+        multi_rarity) %>%
+  filter(estimate != is.na(estimate)) %>%
+  mutate(overunder = ifelse(true_value <= estimate,
+                            "over",
+                            "under"),
+         deviation = ifelse(abs(estimate) > abs(true_value),
+                            abs(estimate) - abs(true_value),
+                            abs(true_value) - abs(estimate))) %>%
+  group_by(measure, method, sample_size, overunder) %>%
+  summarise(dev = mean(abs(deviation), na.rm = TRUE),
+            tally = n()) %>%
+  group_by(measure, method, sample_size) %>%
+  filter(tally == max(tally)) %>%
+  group_by(measure, method) %>%
+  mutate(x = dev/max(dev)) %>%
+  mutate(x = ifelse(overunder == "under",
+                    -1*x,
+                    x))
 
-for (i in 1:2) {
-  
-  dat = data[[i]]
-  
-  overunders = 
-    dat %>%
-    filter(estimate != is.na(estimate)) %>%
-    mutate(overunder = ifelse(true_value <= estimate,
-                              "over",
-                              "under"),
-           deviation = ifelse(abs(estimate) > abs(true_value),
-                              abs(estimate) - abs(true_value),
-                              abs(true_value) - abs(estimate))) %>%
-    group_by(measure, method, sample_size, overunder) %>%
-    summarise(dev = mean(abs(deviation), na.rm = TRUE),
-              tally = n()) %>%
-    group_by(measure, method, sample_size) %>%
-    filter(tally == max(tally)) %>%
-    group_by(measure, method) %>%
-    mutate(x = dev/max(dev)) %>%
-    mutate(x = ifelse(overunder == "under",
-                      -1*x,
-                      x))
-  
-  sim_moon_means =
-    dat %>%
-    filter(estimate != is.na(estimate)) %>%
-    #if true value falls in estimate's CI
-    filter(estimate != is.na(estimate)) %>%
-    mutate(hit = ifelse(ci_low <= true_value & true_value <= ci_high,
-                        2,
-                        1)) %>%
-    group_by(method, measure, sample_size) %>%
-    #calcualte proportion of 'hits' per trait, methods, moment
-    summarise(percentage = sum(hit - 1)/n(),
-              deviation = mean(ifelse(abs(estimate) > abs(true_value),
-                                      abs(estimate) - abs(true_value),
-                                      abs(true_value) - abs(estimate)),
-                               na.rm = TRUE))
-  
-  moon_plots[[i]] <-
-    ggplot(sim_moon_means) +
-    geom_hline(aes(yintercept = 0),
-               color = "grey50",
-               size = .3) +
-    geom_smooth(
-      data = dat %>%
-        filter(estimate != is.na(estimate)) %>%
-        mutate(deviation = ifelse(abs(estimate) > abs(true_value),
-                                  abs(estimate) - abs(true_value),
-                                  abs(true_value) - abs(estimate))),
-      aes(
-        x = sample_size,
-        y = deviation ,
-        color = method),
-      alpha = 0.5,
-      se = FALSE,
-      size = 0.4,
-      linetype = 4) +
-    geom_point(aes(
+sim_moon_means =
+  rbind(multi_method,
+        multi_rarity) %>%
+  filter(estimate != is.na(estimate)) %>%
+  #if true value falls in estimate's CI
+  filter(estimate != is.na(estimate)) %>%
+  mutate(hit = ifelse(ci_low <= true_value & true_value <= ci_high,
+                      2,
+                      1)) %>%
+  group_by(method, measure, sample_size) %>%
+  #calcualte proportion of 'hits' per trait, methods, moment
+  summarise(percentage = sum(hit - 1)/n(),
+            deviation = mean(ifelse(abs(estimate) > abs(true_value),
+                                    abs(estimate) - abs(true_value),
+                                    abs(true_value) - abs(estimate)),
+                             na.rm = TRUE))
+
+moon_plots <-
+  ggplot(sim_moon_means) +
+  geom_hline(aes(yintercept = 0),
+             color = "grey50",
+             size = .3) +
+  geom_smooth(
+    data = rbind(multi_method,
+                 multi_rarity) %>%
+      filter(estimate != is.na(estimate)) %>%
+      mutate(deviation = ifelse(abs(estimate) > abs(true_value),
+                                abs(estimate) - abs(true_value),
+                                abs(true_value) - abs(estimate))),
+    aes(
       x = sample_size,
-      y = deviation,
-      color = method
-    ),
-    size = 1,
-    alpha = 0.9) +
-    geom_moon(aes(
-      x = sample_size,
-      y = deviation,
-      ratio = percentage,
-      fill = method
-    ),
-    color = "transparent",
-    size = 1.2) +
-    scale_fill_manual(guide = guide_legend(title = "Method",
+      y = deviation ,
+      color = method),
+    alpha = 0.5,
+    se = FALSE,
+    size = 0.4,
+    linetype = 4) +
+  geom_point(aes(
+    x = sample_size,
+    y = deviation,
+    color = method
+  ),
+  size = 1,
+  alpha = 0.9) +
+  geom_moon(aes(
+    x = sample_size,
+    y = deviation,
+    ratio = percentage,
+    fill = method
+  ),
+  color = "transparent",
+  size = 1.2) +
+  scale_fill_manual(guide = guide_legend(title = "Method",
+                                         title.position="top"),
+                    values = colorspace::darken(pal_df$c[c(1,2,4)], amount = 0.25),
+                    labels = pal_df$l[c(1,2,4)]) +
+  scale_colour_manual(guide = guide_legend(title = "Method",
                                            title.position="top"),
-                      values = colorspace::darken(pal_df$c[c(1,2,4)], amount = 0.25),
-                      labels = pal_df$l[c(1,2,4)]) +
-    scale_colour_manual(guide = guide_legend(title = "Method",
-                                             title.position="top"),
-                        values = colorspace::lighten(pal_df$c[c(1,2,4)], amount = 0.1),
-                        labels = pal_df$l[c(1,2,4)]) +  
-    facet_grid(rows = vars(measure),
-               cols = vars(method),
-               labeller = labeller(
-                 trait = traits_parsed,
-                 .default = capitalize
-               ),
-               switch = 'y',
-               scales = 'free') +
-    labs(x = "Sample size",
-         y = "Average deviation from true moment") +
-    # Theme
-    theme_moon +
-    theme(
-      axis.ticks = element_blank(),
-      axis.text = element_text(size = rel(.3)),
-      axis.title = element_text(size = rel(.5)),
-      legend.text = element_text(size = rel(.3)),
-      legend.title = element_text(size = rel(.5)),
-      strip.text.y = element_text(margin = margin(0, 0, 3, 0),
-                                  size = rel(.5), face = "bold"),
-      strip.text.x.top = element_text(margin = margin(0, 0, 3, 0),
-                                      size = rel(.5), face = "bold"),
-      panel.grid.major.y = element_line(size = 0.03),
-      strip.background = element_blank(),
-      axis.line = element_blank(),
-      strip.placement = 'outside',
-      panel.background = element_rect(colour = colorspace::lighten("#141438", 0.1),
-                                      size = 0.3),
-      plot.title.position = "panel",
-      plot.title = element_text(margin = margin(0, 0, 10, 0),
-                                size = rel(.7), face = "bold"),
-      legend.position = 'right',
-      plot.margin = margin(2, 2, 2, 2),
-      legend.key.size = unit(3, "mm"),
-      axis.ticks.length=unit(0.25, "mm")
-    )
-  
-  inset_plots[[i]] =
-    ggplot(overunders) +
-    geom_col(aes(y = x,
-                 x = as.factor(sample_size),
-                 fill = method,
-                 group = method),
-             position = 'dodge',
-             alpha = 0.5,
-             show.legend = FALSE) +
-    facet_grid(rows = vars(measure),
-               cols = vars(method),
-               labeller = labeller(
-                 trait = traits_parsed,
-                 .default = capitalize
-               ),
-               switch = 'y')  + 
-    geom_segment(aes(y = 0,
-                     xend = 7.5,
-                     x = 0.5, yend = 0),
-                 colour = 'grey69',
-                 size = 0.2) +
-    scale_fill_manual(values = pal_df$c[c(1,2,4)],
-                      breaks = pal_df$l[c(1,2,4)]) +
-    lims(y = c(-1.3,10))  +
-    scale_x_continuous(trans = 'sqrt', breaks = c(1,4,9,16,25, 36,49),
-                       limits = c(1, 50))+ 
-    expand_limits(x= c(0, 23)) +
-    # Theme
-    theme_void() +
-    theme(
-      strip.text = element_blank()
-    )
-  
-}
+                      values = colorspace::lighten(pal_df$c[c(1,2,4)], amount = 0.1),
+                      labels = pal_df$l[c(1,2,4)]) +  
+  facet_grid(rows = vars(measure),
+             cols = vars(method),
+             labeller = labeller(
+               trait = traits_parsed,
+               .default = capitalize
+             ),
+             switch = 'y',
+             scales = 'free') +
+  labs(x = "Sample size",
+       y = "Average deviation from true moment") +
+  # Theme
+  theme_moon +
+  theme(
+    axis.ticks = element_blank(),
+    axis.text = element_text(size = rel(.3)),
+    axis.title = element_text(size = rel(.5)),
+    legend.text = element_text(size = rel(.3)),
+    legend.title = element_text(size = rel(.5)),
+    strip.text.y = element_text(margin = margin(0, 0, 3, 0),
+                                size = rel(.5), face = "bold"),
+    strip.text.x.top = element_text(margin = margin(0, 0, 3, 0),
+                                    size = rel(.5), face = "bold"),
+    panel.grid.major.y = element_line(size = 0.03),
+    strip.background = element_blank(),
+    axis.line = element_blank(),
+    strip.placement = 'outside',
+    panel.background = element_rect(colour = colorspace::lighten("#141438", 0.1),
+                                    size = 0.3),
+    plot.title.position = "panel",
+    plot.title = element_text(margin = margin(0, 0, 10, 0),
+                              size = rel(.7), face = "bold"),
+    legend.position = 'right',
+    plot.margin = margin(2, 2, 2, 2),
+    legend.key.size = unit(3, "mm"),
+    axis.ticks.length=unit(0.25, "mm")
+  )
 
-layout <- '
-A
-B
-'
+inset_plots =
+  ggplot(overunders) +
+  geom_col(aes(y = x,
+               x = as.factor(sample_size),
+               fill = method,
+               group = method),
+           position = 'dodge',
+           alpha = 0.5,
+           show.legend = FALSE) +
+  facet_grid(rows = vars(measure),
+             cols = vars(method),
+             labeller = labeller(
+               trait = traits_parsed,
+               .default = capitalize
+             ),
+             switch = 'y')  + 
+  geom_segment(aes(y = 0,
+                   xend = 7.5,
+                   x = 0.5, yend = 0),
+               colour = 'grey69',
+               size = 0.2) +
+  scale_fill_manual(values = pal_df$c[c(1,2,4)],
+                    breaks = pal_df$l[c(1,2,4)]) +
+  lims(y = c(-1.3,10))  +
+  scale_x_continuous(trans = 'sqrt', breaks = c(1,4,9,16,25, 36,49),
+                     limits = c(1, 50))+ 
+  expand_limits(x= c(0, 23)) +
+  # Theme
+  theme_void() +
+  theme(
+    strip.text = element_blank()
+  )
 
-((moon_plots[[1]] +
-    labs(tag = "A") +
+
+((moon_plots+
     theme(legend.position = 'right',
           plot.tag = element_text(size = rel(0.7), 
                                   face = "bold")) +
@@ -290,21 +278,8 @@ B
                   bottom = 0.9,
                   right = 0.1,
                   top = 1, 
-                  align_to = 'full') + theme_void()) +
-    (moon_plots[[2]]  +
-       labs(tag = "B") +
-       theme(legend.position = 'right',
-             plot.tag = element_text(size = rel(0.7), 
-                                     face = "bold")) +
-       inset_element(img1,
-                     left = 0.05,
-                     bottom = 0.89,
-                     right = 0.11,
-                     top = 1, 
-                     align_to = 'full') + theme_void())) +
-  plot_layout(guides = 'collect',
-              # heights = c(1, 0.7),
-              design = layout) +
+                  align_to = 'full') + theme_void())) +
+  plot_layout(guides = 'collect') +
   plot_annotation(theme = theme(
     plot.background = element_rect(fill = "white", colour = NA),
     legend.position = 'right')) +
