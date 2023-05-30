08551ef96ef5d10c60923ee3a458922ddf4add4f
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-30 19:25:33 +0200

    :hatching_chick:  initial commit

---
+++ scripts_for_ms_with_traitstrap/2.2_figures_supp.R
@@ -0,0 +1,10 @@
+# Admin ----
+
+#load script that determines plotting aesthetics
+source("scripts_for_ms_with_traitstrap/plotting_aesthetics.R")
+#load function to clean data
+source("r_functions/tidy_simdata.R")
+library(patchwork)
+
+# Figure S1 ----
+## a) load data ----
