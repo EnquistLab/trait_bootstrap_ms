449f46fdffbb5cd3c4b3d739e15cb8a1a258c417
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-31 13:01:52 +0200

    🪆 Fig S9

--- scripts_for_ms_with_traitstrap/2.2_figures_supp.R
+++ scripts_for_ms_with_traitstrap/2.2_figures_supp.R
@@ -873,3 +873,201 @@ ggsave(here::here("figures/Figure_S7.png"),
        height = 160, width = 180,
        units = "mm", dpi = 600)
 # Figure S8 ----
+## a) data ----
+## b) plots ----
+# Figure S9 ----
+## a) data ----
+overunders = 
+  panama_percent %>%
+  filter(estimate != is.na(estimate)) %>%
+  mutate(pct_abd_sampled = round(pct_abd_sampled, digits = -1),
+         overunder = ifelse(true_value <= estimate,
+                            "over",
+                            "under"),
+         deviation = ifelse(abs(estimate) > abs(true_value),
+                            abs(estimate) - abs(true_value),
+                            abs(true_value) - abs(estimate))) %>%
+  group_by(moment, method, pct_abd_sampled, overunder) %>%
+  summarise(dev = mean(deviation, na.rm = TRUE),
+            tally = n()) %>%
+  group_by(moment, method, pct_abd_sampled) %>%
+  filter(tally == max(tally)) %>%
+  group_by(moment, method) %>%
+  mutate(x = dev/max(dev)) %>%
+  mutate(x = ifelse(overunder == "under",
+                    -1*x,
+                    x))
+
+sim_moon_means_panama =
+  panama_percent %>%
+  filter(estimate != is.na(estimate)) %>%
+  mutate(hit = ifelse(ci_low <= true_value & true_value <= ci_high,
+                      2,
+                      1),
+         pct_abd_sampled = round(pct_abd_sampled, digits = -1)) %>%
+  group_by(method, moment, pct_abd_sampled) %>%
+  #calcualte proportion of 'hits' per trait, methods, moment
+  summarise(percentage = sum(hit - 1)/n(),
+            group_size = n(),
+            deviation = mean(ifelse(abs(estimate) > abs(true_value),
+                                    abs(estimate) - abs(true_value),
+                                    abs(true_value) - abs(estimate)),
+                             na.rm = TRUE),
+            avg_error = mean(abs((true_value-estimate)/true_value))*100)
+
+sim_moon_means_panama$moment =
+  ordered(sim_moon_means_panama$moment,levels = c("mean",
+                                                  "variance",
+                                                  "skewness",
+                                                  "kurtosis"))
+
+overunders$moment =
+  ordered(overunders$moment,levels = c("mean",
+                                       "variance",
+                                       "skewness",
+                                       "kurtosis"))
+
+panama_percent$moment =
+  ordered(panama_percent$moment,levels = c("mean",
+                                           "variance",
+                                           "skewness",
+                                           "kurtosis"))
+## b) plots ----
+moons_pa <-
+  ggplot(sim_moon_means_panama) +
+  geom_hline(aes(yintercept = 0),
+             color = "grey50",
+             size = .3) +
+  geom_smooth(
+    data = sim_moon_means_panama,
+    aes(
+      x = pct_abd_sampled,
+      y = avg_error ,
+      color = method),
+    alpha = 0.5,
+    se = FALSE,
+    size = 0.5,
+    linetype = 4) +
+  geom_point(aes(
+    x = pct_abd_sampled,
+    y = avg_error,
+    color = method
+  ),
+  size = 2,
+  alpha = 0.9) +
+  geom_moon(aes(
+    x = pct_abd_sampled,
+    y = avg_error,
+    ratio = percentage,
+    #right = right,
+    fill = method
+  ),
+  color = "transparent",
+  size = 2) +
+  scale_fill_manual(guide = guide_legend(title = "Method",
+                                         title.position="top"),
+                    values = colorspace::darken(pal_df$c, amount = 0.25),
+                    labels = pal_df$l) +
+  scale_colour_manual(guide = guide_legend(title = "Method",
+                                           title.position="top"),
+                      values = colorspace::lighten(pal_df$c, amount = 0.1),
+                      labels = pal_df$l) +  
+  facet_grid(rows = vars(moment),
+             cols = vars(method),
+             labeller = labeller(
+               trait = traits_parsed,
+               .default = capitalize
+             ),
+             switch = 'y',
+             scales = 'free') +
+  labs(x = "Percent cumulative abundance sampled",
+       y = "Average percent error (%)") +
+  # Theme
+  theme_moon +
+  theme(
+    axis.ticks = element_line(size = 0.03),
+    axis.text = element_text(size = rel(.5)),
+    axis.title = element_text(size = rel(.6)),
+    strip.text.y = element_text(margin = margin(0, 0, 3, 0),
+                                size = rel(.7), face = "bold"),
+    strip.text.x.top = element_text(margin = margin(0, 0, 3, 0),
+                                    size = rel(.7), face = "bold"),
+    panel.grid.major.y = element_line(size = 0.03),
+    strip.background = element_blank(),
+    axis.line = element_blank(),
+    #strip.placement = 'outside',
+    panel.background = element_rect(colour = colorspace::lighten("#141438", 0.1),
+                                    size = 0.7),
+    plot.title.position = "panel",
+    legend.position = 'none',
+    plot.margin = margin(7, 19, 10, 15),
+    legend.key.size = unit(3, "mm")
+    
+  )
+
+inset_pa =
+  ggplot(overunders) +
+  geom_col(aes(y = x,
+               x = as.factor(pct_abd_sampled),
+               fill = method,
+               group = method),
+           position = 'dodge',
+           alpha = 0.5,
+           show.legend = FALSE) +
+  facet_grid(rows = vars(moment),
+             cols = vars(method),
+             labeller = labeller(
+               trait = traits_parsed,
+               .default = capitalize
+             ),
+             switch = 'y')  + 
+  geom_segment(aes(y = 0,
+                   xend = 9.5,
+                   x = 0.5, yend = 0),
+               colour = 'grey69',
+               size = 0.3) +
+  scale_fill_manual(values = pal_df$c,
+                    breaks = pal_df$l) +
+  lims(y = c(-1.3,10)) + 
+  expand_limits(x= c(0, 22)) +
+  # Theme
+  theme_void() +
+  theme(
+    strip.text = element_blank()
+  )
+
+moons_pa +
+  inset_element(inset_pa,
+                left = 0,
+                bottom = 0,
+                right = 1,
+                top = 1, 
+                ignore_tag = TRUE) +
+  inset_element(img3,
+                left = 0,
+                bottom = 0.9,
+                right = 0.09,
+                top = 1, 
+                align_to = 'full') + theme_void()
+
+ggsave(here::here("figures/Figure_S9.png"),
+       height = 100, width = 160,
+       units = "mm", dpi = 600)
+# Figure S10 ----
+## a) data ----
+## b) plots ----
+# Figure S11 ----
+## a) data ----
+## b) plots ----
+# Figure S12 ----
+## a) data ----
+## b) plots ----
+# Figure S13 ----
+## a) data ----
+## b) plots ----
+# Figure S14 ----
+## a) data ----
+## b) plots ----
+# Figure S15 ----
+## a) data ----
+## b) plots ----
